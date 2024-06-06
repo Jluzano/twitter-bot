b1923494fbbc7af47421fd000fccb63c2570d852
Justin <justinluzano@gmail.com>
2024-06-06 15:10:07 -0700

    implementing code
    implementing random video retrieval from playlist & thumbnail retrieval testing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tweepy
-
+import tweepy, random, requests
+from pytube import Playlist
+from PIL import Image
+from io import BytesIO
 from config import api_key, api_secret, bearer_token, access_token, access_token_secret
 
 client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
@@ -7,4 +9,25 @@ client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_t
 auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
 
-client.create_tweet(text="testing...")
+#Creating Playlist variable
+p = Playlist('https://www.youtube.com/playlist?list=PLh6Ws4Fpphfqr7VL72Q6HK5Ole9YI54hv')
+print("Loaded playlist.")
+PlaylistVideos = p.videos
+
+#choosing random video
+randomVideo = random.choice(PlaylistVideos)
+
+#getting url
+link = randomVideo.watch_url
+
+#saving thumbnail image
+thumbnail = randomVideo.thumbnail_url
+response = requests.get(thumbnail)
+image = Image.open(BytesIO(response.content))
+image.show()
+
+#Removing text from video title
+ostTitle = randomVideo.title.replace('ブルーアーカイブ Blue Archive ', '')
+
+print(f'The OST chosen is: {ostTitle}')
+print(f'The URL is: {link}')
