4f5a376ae8862c4f17429814a4b9befeee070028
Justin <justinluzano@gmail.com>
2024-06-06 16:52:01 -0700

    fixing issues
    fixed thumbnail local save in memory issue

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ pip install pytube requests pillow
 '''
 def main():
     #initializing API
-    client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
+    client = tweepy.Client(
+        bearer_token, 
+        api_key, 
+        api_secret, 
+        access_token, 
+        access_token_secret)
+
+    #API initialization for Auth 1 API use
+    auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
+    api = tweepy.API(auth)
 
     #Creating Playlist variable
     p = Playlist('https://www.youtube.com/playlist?list=PLh6Ws4Fpphfqr7VL72Q6HK5Ole9YI54hv')
@@ -34,13 +43,16 @@ def main():
     image_io = BytesIO()
     image.save(image_io, "PNG")
     image_io.seek(0)
-    media = client.upload_media(media=image_io, media_category="tweet_image")
+    #uploading thumbnail as media to call as parameter in create_tweet function
+    media = api.media_upload(filename="thumbnail.png", file=image_io)
 
     #Removing text from video title
     ostTitle = randomVideo.title.replace('ブルーアーカイブ Blue Archive ', '')
 
     #creating tweet
-    client.create_tweet(text=f'Today\'s OST is: {ostTitle}\n{link}', media_ids=[media.media_id])
+    client.create_tweet(
+        text=f'Today\'s OST of the day is: {ostTitle}\nLink: {link}', 
+        media_ids=[media.media_id_string])
 
 if __name__ == "__main__":
     main()
